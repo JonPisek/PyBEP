f926e6d479e94cc1eec62b7f9629a8f061b38030
JonPisek <jon.pisek@gmail.com>
2024-03-14 14:51:17 +0100

    main_script update

--- battery_ocv_decomposition/src/main_script.py
+++ battery_ocv_decomposition/src/main_script.py
@@ -5,9 +5,9 @@ from add_battery import load_soc_ocv_data
 # change this so that it's not hardcoded; this should be provided by the user - note that they
 # will not change the main script; the script should only accept arguments and do something with them
 # but it is not intended to be changed by the user
-battery_loc = r'C:\Users\Uporabnik\Desktop\package_directory\battery_ocv_decomposition\battery_data.txt'
 
-directory_name = 'battery_ocv_decomposition'
+battery_loc = r'data\battery_data.txt'
+directory_name = 'data'
 interpolated_cathodes = add_half_cell_data(directory_name + '/cathode_data')
 interpolated_anodes = add_half_cell_data(directory_name + '/anode_data')
 SOC_battery, OCV_battery = load_soc_ocv_data(battery_loc)
@@ -16,4 +16,5 @@ print(interpolated_cathodes)
 print(interpolated_anodes)
 
 # Example usage:
-perform_full_optimization_parallel(SOC_battery, OCV_battery, interpolated_cathodes, interpolated_anodes, iterations=1)
+result = perform_full_optimization_parallel(SOC_battery, OCV_battery, interpolated_cathodes, interpolated_anodes, iterations=1)
+print(result)
